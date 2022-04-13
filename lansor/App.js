0864230acbd8fe5b8f9e530f553c23df85201b02
Serginko <jakubsorad@gmail.com>
2022-04-14 00:02:43 +0200

    app.js fix

--- lansor/App.js
+++ lansor/App.js
@@ -2,21 +2,8 @@ import React, { useState } from 'react';
 import Navigator from './screenStack';
 
 
-<<<<<<< HEAD
 export default function App() {
     return (
       <Navigator />
     );
 }
-=======
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default App
->>>>>>> 39a0e85cc4777470e482243d30ae3f0426619a84
